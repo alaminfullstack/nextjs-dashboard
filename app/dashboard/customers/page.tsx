32c16f9a5add4513f8badbfe80054f5c976c95bc
Softtouch HK <softtouch.humayon@gmail.com>
2024-03-04 20:02:37 +0600

    customer data fatch

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,30 @@
 import { Metadata } from 'next';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { CustomerTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+       <Suspense fallback={<CustomerTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  )
 }
